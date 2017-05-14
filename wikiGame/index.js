725c78dfbafd1b57b4d5acb95c0b97cb7b78b976
Nataraj <nbnataraj7@gmail.,com>
2017-05-14 18:31:47 +0530

    Add timer panel

--- wikiGame/index.js
+++ wikiGame/index.js
@@ -3,7 +3,9 @@ _wiki_game = {
 	"0": "https://en.wikipedia.org/wiki/Biology",
 	"1": "https://en.wikipedia.org/wiki/Computer_science",
 	"2": "https://en.wikipedia.org/wiki/Physics",
-	"3": "https://en.wikipedia.org/wiki/Chemistry"
+	"3": "https://en.wikipedia.org/wiki/Chemistry",
+	"targetWiki": null,
+	"clicks": []
 }
 
 
@@ -32,13 +34,37 @@ var play = function(){
 	window.location.href += "#game";
 	
 	var activeIndex = $("#targetWiki .carousel-indicators .active").attr("data-slide-to");
-	var initialWikiUrl = _wiki_game[activeIndex];
-	$("#wikiFrame").attr("src", initialWikiUrl);
+	_wiki_game.targetWiki = _wiki_game[activeIndex];
 }
 
-var playWithTimer = function(){
-	
+var startTimer = function(time){
+	var seconds = 60;
+	var hours = Math.floor(Math.abs(time/60-1));
+	var minutes = time-1<=0?0:time-1;
+	var milliseconds = seconds * 1000;
+	_wiki_game.timerRef = setInterval(function(){
+		seconds--;
+		if(minutes && seconds == 0){
+			seconds = 59;
+			minutes--;
+		}
+		if(hours && minutes == 0){
+			minutes = 59;
+			hours--;
+		}
+		if(seconds == 0 && minutes == 0 && hours == 0){
+			clearInterval(_wiki_game.timerRef);
+			$("#iframePanel").hide();
+			$("gameCover").show();
+		}
+		hours = hours?"00":hours;
+		minutes = minutes == 0?"00":minutes;
+		seconds = seconds == 0?"00":seconds;
+		var displayTime = hours+":"+minutes+":"+seconds;
+		$("#timerPanel").html("Time&nbsp;&nbsp;<span>"+displayTime+"</span>");
+	}, 1000);
 }
+
 $(document).ready(function(){
 	
 	//some init settings
@@ -53,6 +79,8 @@ $(document).ready(function(){
 		if(time && time > 0){
 			play();
 			$("#timerDialog").modal('toggle');
+			startTimer(time);
+			$("#timerPanel").addClass("timer-panel-show");
 		}
 		else{
 			$("#startTimer").addClass("btn-error");
